522de4ad4c5904668dca7e1a36eda89e12248142
ITNikolov <ivaylonikolov.n@gmail.com>
2025-01-19 22:05:37 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCIdentityRegLoginExercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
